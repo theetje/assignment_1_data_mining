44cc1cdeb31db68c9693b8eaae9d0aed486527c7
Craig Leek <cqhd.leek@gmail.com>
2018-10-10 18:19:59 +0200

    Random attribute selection

--- project/main.R
+++ project/main.R
@@ -1,7 +1,7 @@
 source("helpers.R")
 credit.data <- read.csv("./credit.txt")
 class <- credit.data[, 6]
-data <- credit.data[, -6]
+data <- credit.data[,-6]
 
 root <-
   setRefClass("root", fields = list(left = "ANY", right = "ANY"))
@@ -14,68 +14,76 @@ node <-
       attribute = "ANY",
       value = "ANY"
     ),
-    methods = list(classify = function(x) {
-      if(x[attribute] > value)
-        return(left$classify(x))
-      else
-        return(right$classify(x))
-    })
+    methods = list(
+      classify = function(x) {
+        print(attribute)
+        print(x[attribute])
+        print(value)
+        if (x[attribute] > value)
+          return(left$classify(x))
+        else
+          return(right$classify(x))
+      }
+    )
   )
 leaf <-
   setRefClass(
     "leaf",
     fields = list(data_set = "ANY"),
-    methods = list(classify = function(x) {
-      return(data_set)
-    })
+    methods = list(
+      classify = function(x) {
+        return(data_set)
+      }
+    )
   )
 
 tree.grow <- function(x, y, nmin, minleaf, nfeat) {
-  if (impurity(y) == 0 || length(y) < minleaf) {
-    return(leaf(data_set = as.integer(names(which.max(table(y))))))
+  print(x)
+  if (impurity(y) == 0 || length(y) < nmin) {
+    return(leaf(data_set = as.integer(names(
+      which.max(table(y))
+    ))))
   }
   impurityValue <- impurity(y)
   attributeName <- NULL
   attribute <- NULL
   attributeValue <- NULL
   names <- colnames(data)
-  i <- 1
-  for (a in x) {
-    if (length(unique(a)) > 1) {
-      s <- bestsplit(a, y)
+  predictors <- sample(names, nfeat)
+  print(data$"gender")
+  print(predictors)
+  for (a in predictors) {
+    if (length(unique(x[[a]])) > 1) {
+      s <- bestsplit(x[[a]], y)
       if (s[2] < impurityValue) {
         impurityValue <- s[2]
-        attribute <- a
-        attributeName <- names[i]
+        attribute <- x[[a]]
+        attributeName <- a
         attributeValue <- s[1]
       }
     }
-    i <- i + 1
   }
   if (!is.null(attribute))
   {
     df <- data.frame(x, value = y)
     s.star <- split(df, attribute < attributeValue)
-    left.node <-
-      tree.grow(s.star$F[1], s.star$F$value, nmin, minleaf, nfeat)
-    right.node <-
-      tree.grow(s.star$T[1], s.star$T$value, nmin, minleaf, nfeat)
-    
     return(
       node(
-        left = left.node,
-        right = right.node,
         attribute = attributeName,
-        value = attributeValue
+        value = attributeValue,
+        left = tree.grow(s.star$F[1], s.star$F$value, nmin, minleaf, nfeat),
+        right = tree.grow(s.star$T[1], s.star$T$value, nmin, minleaf, nfeat)
       )
     )
   }
-  return(leaf(data_set = as.integer(names(which.max(table(y))))))
+  return(leaf(data_set = as.integer(names(
+    which.max(table(y))
+  ))))
 }
 
 
-tree <- tree.grow(data, class, 1, 1, 1)
+tree <- tree.grow(data, class, 1, 1, 5)
 
 print(tree)
 
-print(tree$classify(data[7,]))
+print(tree$classify(data[7, ]))
