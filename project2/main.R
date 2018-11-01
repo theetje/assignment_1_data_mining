412e831b1e6939ed7472acaedac53080c5b8f6fb
Craig Leek <cqhd.leek@gmail.com>
2018-11-01 12:38:04 +0100

    Split to functions to make it easier to run multiple times

--- project2/main.R
+++ project2/main.R
@@ -3,6 +3,7 @@ library(glmnet)
 library(rpart)
 library(rpart.plot)
 library(randomForest)
+library(entropy)
 
 train.mnb <- function (dtm,labels) 
 {
@@ -91,56 +92,11 @@ test.all <- tm_map(test.all,stripWhitespace)
 test.terms <- DocumentTermMatrix(test.all,
                                  control=list(dictionary = dimnames(terms)[[2]]))
 
-# term frequencies on the test set
-test3.dtm <- as.matrix(test.terms)
-
-train.glmnet <- cv.glmnet(as.matrix(terms),train.labels,
-                             family="binomial",type.measure="class")
-
-train.logreg2.pred <- predict(train.glmnet,
-                                newx=test3.dtm,s="lambda.min",type="class")
-# show confusion matrix
-log.res <- table(train.logreg2.pred,test.labels)
-
-library(entropy)
 # compute mutual information of each term with class label
 train.mi <- apply(as.matrix(terms),2,
                     function(x,y){mi.plugin(table(x,y)/length(y))},train.labels)
 # sort the indices from high to low mutual information
 train.mi.order <- order(train.mi,decreasing=T)
-# show the five terms with highest mutual information
-train.mi[train.mi.order[1:5]]
-
-
-reviews.mnb <- train.mnb(as.matrix(terms)[,train.mi.order[1:40]],train.labels)
-# create document term matrix for test set
-test.dtm <- DocumentTermMatrix(test.all,
-                                 list(dictionary=dimnames(terms)[[2]]))
-dim(test.dtm)
-reviews.mnb.pred <- predict.mnb(reviews.mnb,as.matrix(test.dtm)[,train.mi.order[1:40]])
-bay.res <- table(reviews.mnb.pred,test.labels)
-
-
-reviews.rpart <- rpart(label~.,
-                         data=data.frame(as.matrix(terms),label=train.labels),
-                         cp=0,method="class")
-# simple tree for plotting
-reviews.rpart.pruned <- prune(reviews.rpart,cp=1.37e-02)
-plot(reviews.rpart.pruned)
-# tree with lowest cv error
-reviews.rpart.pruned <- prune(reviews.rpart,cp=0.001)
-# make predictions on the test set
-reviews.rpart.pred <- predict(reviews.rpart.pruned,
-                                newdata=data.frame(as.matrix(test.dtm)),type="class")
-# show confusion matrix
-tree.res <- table(reviews.rpart.pred,test.labels)
-
-model.rf <- randomForest(as.factor(label)~.,
-                         data=data.frame(as.matrix(terms),label=train.labels), importance = TRUE)
-
-rf.predict <- predict(model.rf, newdata=data.frame(as.matrix(test.dtm)), type = "class")
-
-rf.res <- table(rf.predict, test.labels)
 
 result <- function(table){
   acc <- (table[1,1] + table[2,2])/sum(table)
@@ -153,5 +109,42 @@ result <- function(table){
   print(f1)
 }
 
-result(log.res)
-result(bay.res)
+logres <- function(train, train.labels, test, test.labels){
+  train.glmnet <- cv.glmnet(as.matrix(train),train.labels,
+                            family="binomial",type.measure="class")
+  
+  train.logreg2.pred <- predict(train.glmnet,
+                                newx=test,s="lambda.min",type="class")
+  return(table(train.logreg2.pred,test.labels))
+}
+
+naibay <- function(train, train.labels, features, test, test.labels){
+  reviews.mnb <- train.mnb(as.matrix(train)[,features[1:40]],train.labels)
+  reviews.mnb.pred <- predict.mnb(reviews.mnb,as.matrix(test)[,features[1:40]])
+  return(table(reviews.mnb.pred,test.labels))
+}
+
+tree <- function(train, train.labels, test, test.labels){
+  reviews.rpart <- rpart(label~.,
+                         data=data.frame(as.matrix(train),label=train.labels),
+                         cp=0,method="class")
+  # simple tree for plotting
+  reviews.rpart.pruned <- prune(reviews.rpart,cp=1.37e-02)
+  plot(reviews.rpart.pruned)
+  # tree with lowest cv error
+  reviews.rpart.pruned <- prune(reviews.rpart,cp=0.001)
+  # make predictions on the test set
+  reviews.rpart.pred <- predict(reviews.rpart.pruned,
+                                newdata=data.frame(as.matrix(test)),type="class")
+  # show confusion matrix
+  return(table(reviews.rpart.pred,test.labels))
+}
+
+rf <- function(train, train.labels, test, test.labels){
+  model.rf <- randomForest(as.factor(label)~.,
+                           data=data.frame(as.matrix(train),label=train.labels), importance = TRUE)
+  
+  rf.predict <- predict(model.rf, newdata=data.frame(as.matrix(test)), type = "class")
+  
+  rf.res <- table(rf.predict, test.labels)
+}
